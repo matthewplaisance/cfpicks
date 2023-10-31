a1496b94cca22148adca9bf70a50ea3bd97db3e4
Matthew Plaisance <mplaisance128@gmail.com>
2023-10-31 11:24:49 -0500

    season update

--- src/js/master.js
+++ src/js/master.js
@@ -282,6 +282,7 @@ weeks.forEach(w => {
 
 select("#weekSelect").on("change", () => {
     let week = select("#weekSelect").node().value;
+    if (week == 'default') week = "Week 10";
     weekEl.textContent = week
     week = week.replace(' ','').toLocaleLowerCase()
     const table = document.getElementById('tbody');
