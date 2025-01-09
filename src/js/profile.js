f58284600b0725df50bb47871cdbfd6a8abbb568
Matthew Plaisance <mplaisance128@gmail.com>
2025-01-09 12:05:20 -0600

    week update

--- src/js/profile.js
+++ src/js/profile.js
@@ -146,7 +146,7 @@ function createCard(data,game,wrapper,gn){
     cardBodyDiv.appendChild(clearfixDiv);
     cardBodyDiv.appendChild(dateP);
     if (data.hasOwnProperty("away")){
-        for (let i = 1; i <= NUM_GAMES; i++) {
+        for (let i = 2; i <= NUM_GAMES; i +=2) {
             const boxDiv = document.createElement('div');
             boxDiv.className = 'box';
             boxDiv.textContent = i;
@@ -217,7 +217,7 @@ if (userData){
 }
 
 let pointsPicked = [];
-const cfpweek2 = ['game5','game6','game7','game8']
+const cfpweek2 = ['game9','game10']
 for (let game in picks) {
     if (cfpweek2.includes(game)) {
         pointsPicked.push(String(picks[game].points))
