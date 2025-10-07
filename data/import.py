b8bd17a44cd657ffd7f5aa291763304a9ecbc80d
Matthew Plaisance <mplaisance128@gmail.com>
2025-10-07 17:20:57 -0500

    week7

--- data/import.py
+++ data/import.py
@@ -25,12 +25,12 @@ for obj in data:
     del obj['game']
     res[game] = obj
 
-tb_time_str = "2025-10-04 14:30:00"
+tb_time_str = "2025-10-11 14:30:00"
 dt_local = cdt.localize(datetime.strptime(tb_time_str, "%Y-%m-%d %H:%M:%S"))
 tb_unix = int(dt_local.astimezone(pytz.utc).timestamp())
 
 res["tiebreaker"] = {
-    "home": "Passing Yard for DJ Lagway?",
+    "home": "Texas will beat OU by x Points (Negative for losses by)?",
     "time": tb_unix,
 #    "humanDate": dt_local.strftime("%b %d %H:%M %Z")
     "humanDate": dt_local.strftime("%b %d %H:%M %Z")
