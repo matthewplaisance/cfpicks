0de2348473cd80c817d80948e29099f403e930a5
Matthew Plaisance <mplaisance128@gmail.com>
2026-09-17 09:57:21 -0500

    week3

--- data/import.py
+++ data/import.py
@@ -25,12 +25,12 @@ for obj in data:
     del obj['game']
     res[game] = obj
 
-tb_time_str = "2026-09-12 18:30:00"
+tb_time_str = "2026-09-19 18:30:00"
 dt_local = cdt.localize(datetime.strptime(tb_time_str, "%Y-%m-%d %H:%M:%S"))
 tb_unix = int(dt_local.astimezone(pytz.utc).timestamp())
 
 res["tiebreaker"] = {
-    "home": "How many points will Texas win by? (Negative for losses by)",
+    "home": "How many points will LSU win by? (Negative for losses by)",
     "time": tb_unix,
 #    "humanDate": dt_local.strftime("%b %d %H:%M %Z")
     "humanDate": dt_local.strftime("%b %d %H:%M %Z")
